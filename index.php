e87e92501c3fa54efd4d65086d2a321fc39c2ce1
João Paulo <jpmb1235@gmail.com>
2025-11-29 23:53:13 -0300

    Corrigindo erros

--- index.php
+++ index.php
@@ -1,17 +1,48 @@
 <?php
+// login.php
 
 session_start();
 
 $valid_username = "admin";
+
 $valid_password = "novaSenha";
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"] ?? '';
     $password = $_POST["password"] ?? '';
-    
-    if ($username === $valid_username && $password === $valid_password){
+
+    if ($username === $valid_username && $password === $valid_password) {
         $_SESSION["loggedin"] = true;
         $_SESSION["username"] = $username;
+        echo "Login realizado com sucesso!";
+    } 
+    
+    else {
+
+        echo "Usuário ou senha inválidos.";
     }
- #teste1
 }
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Simplão</title>
+</head>
+
+<body>
+    
+    <h2>login</h2>
+
+    <form method="post" action="">
+        <label>Usuário:</label><br>
+        <input type="text" name="username"><br>
+        <label>Senha:</label><br>
+        <input type="password" name="password"><br><br>
+        <input type="submit" value="Entrar">
+    </form>
+    
+</body>
+</html>
